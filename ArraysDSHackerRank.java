df0689e068311480db088b720df8ef798cfa2569
Your NameKariba <you@example.comkaribayasmin@gmail.com>
2020-08-21 20:05:47 +0600

    1 number Problem solving revised and some missing word of Problem statement added.

--- ArraysDSHackerRank.java
+++ ArraysDSHackerRank.java
@@ -2,26 +2,27 @@
 
 ProblemLink: https://www.hackerrank.com/challenges/arrays-ds/problem?h_r=profile
 
-An array is a type of data structure that stores elements of the same type in a contiguous block of memory. In an array, , of size , each memory location has some unique index,  (where ), that can be referenced as  (you may also see it written as ).
+An array is a type of data structure that stores elements of the same type in a contiguous block of memory. In an array, A, of size N, each memory location has some unique index,i(where 0 <= i < N), that can be referenced as A[i](you may also see it written as Ai).
 
-Given an array, , of  integers, print each element in reverse order as a single line of space-separated integers.
+Given an array, A, of N integers, print each element in reverse order as a single line of space-separated integers.
 
 Note: If you've already solved our C++ domain's Arrays Introduction challenge, you may want to skip this.
 
 Input Format
 
-The first line contains an integer,  (the number of integers in ).
-The second line contains  space-separated integers describing .
+The first line contains an integer, N (the number of integers in A).
+The second line contains N space-separated integers describing A.
 
 Constraints
+1 <= N <= 10^3
+1 <= Ai <= 10^4, where Ai is the ith integer in A
 
 Output Format
 
-Print all  integers in  in reverse order as a single line of space-separated integers.
+Print all N integers in A in reverse order as a single line of space-separated integers.
 
 Sample Input 1
 
-CopyDownload
 Array: arr
 1
 4
@@ -31,6 +32,7 @@ Array: arr
  
 4
 1 4 3 2
+
 Sample Output 1
 
 2 3 4 1 */
